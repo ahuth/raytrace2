571d95edf02dc2c3756f6d44733f836567a7b85b
Andrew Huth <andrew@huth.me>
2020-06-05 09:05:51 -0700

    Account for total internal reflection
    https://raytracing.github.io/books/RayTracingInOneWeekend.html\#dielectrics/totalinternalreflection

--- src/materials/Dialectric.ts
+++ src/materials/Dialectric.ts
@@ -16,14 +16,24 @@ export default class Dialectric extends Material {
   }
 
   scatter(ray: Ray, hit: HitRecord) {
+    // Attenuation is always 1, 1, 1, because the surface absorbs nothing.
+    const attenuation = new Color(1, 1, 1);
+
     const etaI_over_etaT = hit.frontFace ? 1 / this.refractiveIndex : this.refractiveIndex;
+    const cosineTheta = Math.min(ray.direction.unit().negate().dotProduct(hit.normal), 1);
+    const sineTheta = Math.sqrt(1 - cosineTheta * cosineTheta);
+
+    if (etaI_over_etaT * sineTheta > 1) {
+      // Refraction is not possible. Use reflection, instead.
+      const reflected = ray.direction.reflect(hit.normal);
+      const scattered = new Ray(hit.point, reflected);
+
+      return { attenuation, ray: scattered };
+    }
+
     const refracted = ray.direction.refract(hit.normal, etaI_over_etaT);
     const scattered = new Ray(hit.point, refracted);
 
-    return {
-      // Attenuation is 1, 1, 1, because the surface absorbs nothing.
-      attenuation: new Color(1, 1, 1),
-      ray: scattered,
-    };
+    return { attenuation, ray: scattered };
   }
 }
