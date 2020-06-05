43c7c78f695bca725af86250848f25ca9ba7aff4
Andrew Huth <andrew@huth.me>
2020-06-05 09:15:07 -0700

    Add Schlick approximation and potentially reflect (instead of refract) rays
    https://raytracing.github.io/books/RayTracingInOneWeekend.html\#dielectrics/schlickapproximation

--- src/materials/Dialectric.ts
+++ src/materials/Dialectric.ts
@@ -23,8 +23,19 @@ export default class Dialectric extends Material {
     const cosineTheta = Math.min(ray.direction.unit().negate().dotProduct(hit.normal), 1);
     const sineTheta = Math.sqrt(1 - cosineTheta * cosineTheta);
 
+    // Check if refraction is possible. If not, use reflection.
     if (etaI_over_etaT * sineTheta > 1) {
-      // Refraction is not possible. Use reflection, instead.
+      const reflected = ray.direction.reflect(hit.normal);
+      const scattered = new Ray(hit.point, reflected);
+
+      return { attenuation, ray: scattered };
+    }
+
+    const probabilityOfReflection = schlick(cosineTheta, etaI_over_etaT);
+
+    // Based on the angle and material, there's a chance that the ray will be reflected and not
+    // refracted.
+    if (Math.random() < probabilityOfReflection) {
       const reflected = ray.direction.reflect(hit.normal);
       const scattered = new Ray(hit.point, reflected);
 
@@ -36,4 +47,14 @@ export default class Dialectric extends Material {
 
     return { attenuation, ray: scattered };
   }
+}
+
+/**
+ * Schlick approximation.
+ * @see https://raytracing.github.io/books/RayTracingInOneWeekend.html#dielectrics/schlickapproximation
+ */
+function schlick(cosine: number, refractiveIndex: number) {
+  const r0 = (1 - refractiveIndex) / (1 + refractiveIndex);
+  const r0_squared = r0 * r0;
+  return r0_squared + (1 - r0_squared) * Math.pow(1 - cosine, 5);
 }
